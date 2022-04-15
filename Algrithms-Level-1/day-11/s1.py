29b545a03ce177b18f5e7146909b0dc435b9b3f7
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-15 22:48:43 +0200

    Q1 of day-11 python

--- Algrithms-Level-1/day-11/s1.py
+++ Algrithms-Level-1/day-11/s1.py
@@ -5,3 +5,11 @@ from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         return list(combinations(range(1, n+1), k)
+
+# Solution 2:
+# adopted from @StefanPochmann
+# class Solution:
+#     def combine(self, n, k):
+#         if k == 0:
+#             return [[]]
+#         return [pre + [i] for i in range(k, n+1) for pre in self.combine(i-1, k-1)]
