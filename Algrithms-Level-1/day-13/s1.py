7519999209e213e258b585b428813d9c0be932c5
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-17 13:43:13 +0200

    Q1 of day-13: Python

--- Algrithms-Level-1/day-13/s1.py
+++ Algrithms-Level-1/day-13/s1.py
@@ -2,3 +2,5 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         s = bin(n)[2:]
         return True if s[0] == '1' and s.count('1') == 1 else False
+        # Solution 2
+        # return n and not (n & n - 1)
