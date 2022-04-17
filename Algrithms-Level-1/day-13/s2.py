5fa78c7c60f42edb3b130fd154e93d3c33f7fbb4
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-17 13:40:27 +0200

    Q2 of day-13: Python

--- Algrithms-Level-1/day-13/s2.py
+++ Algrithms-Level-1/day-13/s2.py
@@ -3,3 +3,10 @@ class Solution:
         # bin() built-in func converts integer to string in 0b1011 (n=3)
         # count() finds how many times "1" occurs in the string
         return bin(n).count('1', 2)
+
+        # solution 2
+        # res = 0
+        # while n:
+        #     n &= n - 1
+        #     res += 1
+        # return res
