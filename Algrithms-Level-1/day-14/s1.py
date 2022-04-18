72761e7e15184815da5879698a972502b7c16246
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-18 11:29:34 +0200

    changed Q1 of day-14

--- Algrithms-Level-1/day-14/s1.py
+++ Algrithms-Level-1/day-14/s1.py
@@ -1,3 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         return int("{:032b}".format(n)[::-1], 2)
+
+# solution 2
+    # def reverseBits(self, n):
+    #     ans = 0
+    #     for i in xrange(32):
+    #         ans = (ans << 1) + (n & 1)
+    #         n >>= 1
+    #     return ans
