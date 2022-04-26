9af4561f04c19ca7c060e48b7d30a4bf8ecc6569
Otabek8866 <sobirovotabek1995@gmail.com>
2022-04-26 21:10:18 +0200

    Q1 of day-4: C

---
+++ dynamic-programming-level-1/day-4/s1.c
@@ -0,0 +1,9 @@
+bool canJump(int* nums, int numsSize){
+    int jump = 0;
+    for (int i=0; i<numsSize; i++){
+        if (jump < i)
+            return false;
+        jump = (jump > i+nums[i]) ? jump : i+nums[i];
+    }
+    return true;
+}
