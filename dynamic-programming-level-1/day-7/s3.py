292df2820e2804a71564e1995519716f2145c4a2
Otabek8866 <sobirovotabek1995@gmail.com>
2022-05-01 11:03:38 +0200

    Q3 of day-7: Python, solution 2

--- dynamic-programming-level-1/day-7/s3.py
+++ dynamic-programming-level-1/day-7/s3.py
@@ -19,3 +19,9 @@ class Solution:
             right += 1
         total += profit
         return total
+
+        # total = 0
+        # for i in range(1, len(prices)):
+        #     if prices[i] > prices[i-1]:
+        #         total += prices[i] - prices[i-1]
+        # return total
